13db7caa929f4d1e1b06dc0babe5f8c205afeadd
Sudarshan <sdrshnnmbr@gmail.com>
2024-01-22 16:48:08 -0500

    Add .gitignore, LICENSE, and requirements.txt files
    Update settings.py
    
    Remove unnecessary h3 tag from room.html

--- djitter/settings.py
+++ djitter/settings.py
@@ -12,15 +12,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)m*m3=9&9cjcrz=p7i%&odt=k)u7jg!_^byp8t7g+4=3w&^5eq'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-)m*m3=9&9cjcrz=p7i%&odt=k)u7jg!_^byp8t7g+4=3w&^5eq')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +81,7 @@ WSGI_APPLICATION = 'djitter.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / config('DB_NAME', default='db.sqlite3'),
     }
 }
 
